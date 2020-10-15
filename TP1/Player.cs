00e19a5faebf1a1e943aa90459065aa3b5cd960d
Nathanael-ROVERE <nathanaelrovere@gmail.com>
2020-10-15 17:30:31 +0200

    TP1 with bonus

--- TP1/Player.cs
+++ TP1/Player.cs
@@ -6,25 +6,39 @@ public class Player : MonoBehaviour {
     // VARIABLES
     public float vitesseDeplacement;
     public float vitesseRotation;
+    private float acceleration = 0;
+    public float accelerationSpeed;
+    private Vector3 mouvement = Vector3.zero;
 
     // FONCTIONS
-    void Start() {
-        vitesseDeplacement = 30;
-        vitesseRotation = 50;
-    }
-
     void Update() {
+        if ((Input.GetKey(KeyCode.UpArrow)) || (Input.GetKey(KeyCode.DownArrow))) {
+            acceleration++;
+            transform.Translate(Vector3.forward * Time.deltaTime * accelerationSpeed);
+            if (acceleration>vitesseDeplacement) {acceleration=vitesseDeplacement;}
+        }
+        else if ((!Input.GetKey(KeyCode.UpArrow)) && (!Input.GetKey(KeyCode.DownArrow))) {
+            acceleration--;
+            transform.Translate(Vector3.forward * Time.deltaTime * accelerationSpeed);
+            if (acceleration<0) {acceleration=0;}
+        }
+
         if (Input.GetKey(KeyCode.UpArrow)) {
-            transform.Translate(Vector3.forward * Time.deltaTime * vitesseDeplacement);
+            //transform.Translate(Vector3.forward * Time.deltaTime * acceleration);
+            mouvement = Vector3.forward;
         }
         if (Input.GetKey(KeyCode.DownArrow)) {
-            transform.Translate(-Vector3.forward * Time.deltaTime * vitesseDeplacement);
+            //transform.Translate(-Vector3.forward * Time.deltaTime * acceleration);
+            mouvement = -1f * Vector3.forward;
         }
+        transform.Translate(mouvement * Time.deltaTime * acceleration);
+
         if (Input.GetKey(KeyCode.RightArrow)) {
             transform.Rotate(Vector3.up * Time.deltaTime * vitesseRotation);
         }
         if (Input.GetKey(KeyCode.LeftArrow)) {
             transform.Rotate(-Vector3.up * Time.deltaTime * vitesseRotation);
         }
+        if (Input.GetKey("escape")) {Application.Quit();}
     }
 }
